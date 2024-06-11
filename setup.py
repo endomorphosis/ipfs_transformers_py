4964470aba395480f97340a20d68499d75bbcb50
endomorphosis <starworks5@gmail.com>
2024-06-11 00:40:55 -0700

    chore: Remove commented out dependency in setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,6 @@ setup(
         'ipfs_transformers',
 	],
 	install_requires=[
-        # 'ipfs_datasets@git+https://github.com/endomorphosis/ipfs_datasets.git',
         'orbitdb_kit@git+https://github.com/endomorphosis/orbitdb_kit.git',
         'ipfs_kit@git+https://github.com/endomorphosis/ipfs_kit.git',
         'ipfs_model_manager@git+https://github.com/endomorphosis/ipfs_model_manager.git',
